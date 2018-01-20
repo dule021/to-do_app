01d9b276b6c23cf56b6dfc05c93e0069f96b9d47
Dusan Petrovic <dule021@gmail.com>
2018-01-20 21:00:22 +0100

    editing possible, undo added, css added

--- todo_app.js
+++ todo_app.js
@@ -4,32 +4,50 @@ var content = document.getElementById("msg");
 var newNotes = document.getElementById("newNotes");
 var doneNotes = document.getElementById("doneNotes");
 
+var handler;
+
 
 testButton.addEventListener("click", function(){
     var newDiv = document.createElement("DIV");
     var newParagraph = document.createElement("P");
     
     var newDeleteButton = document.createElement("BUTTON");
-    newDeleteButton.innerText = "DELETE";
+    newDeleteButton.innerText = "delete";
     newDeleteButton.setAttribute("class", "deleteButton");
     newDeleteButton.addEventListener("click", function(){
         newDeleteButton.parentNode.remove();
     })
     
     var newDoneButton = document.createElement("BUTTON");
-    newDoneButton.innerText = "DONE";
+    newDoneButton.innerText = "done";
     newDoneButton.setAttribute("class", "doneButton");
     newDoneButton.addEventListener("click", function() {
+       newDoneButton.remove();
+       newDiv.appendChild(undoButton);
        doneNotes.appendChild(newDiv);
-       newNotes.removeChild(newDiv);
+       newNotes.removeChild(newDiv);    
     })
 
-    var colorPicker = document.createElement("INPUT");
-    colorPicker.setAttribute("type", "color");
-  
-    colorPicker.addEventListener("change", function(){
+    var undoButton = document.createElement("BUTTON");
+    undoButton.innerText = "undo";
+    undoButton.setAttribute("class", "undoButton");
+    undoButton.addEventListener("click", function() {
+        undoButton.remove();
+        newDiv.appendChild(newDoneButton);
+        newNotes.appendChild(newDiv);
+        doneNotes.removeChild(newDiv);
+    })
+
+
+    var color = document.createElement("INPUT");
+    color.setAttribute("type", "color");
+    color.addEventListener("change", function(){
         var newColor = colorPicker.value;
         newDiv.style.backgroundColor = newColor;
+    })
+    var colorPicker = document.createElement("BUTTON");
+    colorPicker.addEventListener("click", function() {
+        return color;
     })
     
     newDiv.appendChild(newParagraph);
@@ -37,11 +55,78 @@ testButton.addEventListener("click", function(){
     newDiv.appendChild(newDoneButton);
     newDiv.appendChild(colorPicker);
     newDiv.setAttribute("class", "newDiv");
-    
 
- 
-    
-    newParagraph.innerHTML = content.value;
+
+    handler = function change(event) {
+        
+            var editContent = document.createElement("textarea");
+            editContent.value = event.target.innerText;
+            //event.target.innerHTML = null;
+            //event.target.appendChild(editContent);
+            
+            var parent = event.target.parentNode;
+            parent.appendChild(editContent);
+            parent.removeChild(newDeleteButton);
+            if(parent.parentNode==newNotes){
+            parent.removeChild(newDoneButton);
+            }
+            else{
+                parent.removeChild(undoButton)
+            };
+            parent.removeChild(colorPicker);
+
+            var saveButton = document.createElement("button");
+            saveButton.innerText = "SAVE";
+            saveButton.addEventListener("click", function() {
+                var newParagraph = document.createElement("P");
+                newParagraph.innerText = editContent.value;
+                parent.removeChild(editContent);
+                parent.removeChild(saveButton);
+                parent.appendChild(newParagraph);
+                parent.appendChild(newDeleteButton);
+                if(parent.parentNode==newNotes){
+                parent.appendChild(newDoneButton);
+                }
+                else{
+                    parent.appendChild(undoButton)
+                };
+                parent.appendChild(colorPicker);
+                newParagraph.addEventListener("click", handler);
+            })
+            parent.appendChild(saveButton);
+            event.target.remove();
+    }
+
+    newParagraph.addEventListener("click", handler);
+    newParagraph.innerText = content.value;
     newNotes.appendChild(newDiv);
-    
 })
+
+doneNotes.style.display = "none";
+
+
+
+
+var toDoAreaButton = document.getElementById("toDoAreaButton");
+toDoAreaButton.addEventListener("click", function() {
+    doneNotes.style.display = "none";
+    newNotes.style.display = "flex";
+
+    toDoAreaButton.style.backgroundColor = "#FF8C00";
+    doneAreaButton.style.backgroundColor = "white";
+    toDoAreaButton.style.fontWeight = "bold";
+    doneAreaButton.style.fontWeight = "normal";
+})
+
+var doneAreaButton = document.getElementById("doneAreaButton");
+doneAreaButton.addEventListener("click", function() {
+    newNotes.style.display = "none";
+    doneNotes.style.display = "flex";
+    toDoAreaButton.style.backgroundColor = "white";
+    doneAreaButton.style.backgroundColor = "#FF8C00";
+    toDoAreaButton.style.fontWeight = "normal";
+    doneAreaButton.style.fontWeight = "bold";
+})
+
+toDoAreaButton.style.backgroundColor = "#FF8C00";
+toDoAreaButton.style.fontWeight = "bold";
